7c42d69b87198f7952e4df8e468c11cb5a23f3c1
Dolma <dolma1918@gmail.com>
2024-08-13 21:28:05 +0545

    feat: IBiblioALiases

---
+++ IBiblioALiases.java
@@ -0,0 +1,17 @@
+import java.net.*;
+public class IBiblioALiases {
+    public static void main(String[] args) {
+        try {InetAddress ibiblio=InetAddress.getByName("www.ibiblio.org");
+        InetAddress helios=InetAddress.getByName("www.facebook.com.");
+        if(ibiblio.equals(helios)) {
+            System.out.println("www.ibiblio.org is the same as helios.ibiblio.org");
+        }
+        
+        else {
+            System.out.println("www.ibiblio.org is not the same as helios.ibiblio.org");    
+        }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);  
+        }
+    }
+}
